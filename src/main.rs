54ca3831763bffcf29364224aaac8f3c8dc22c04
lebbion <145786435+lebbion@users.noreply.github.com>
2023-09-22 18:05:49 +0300

    prepare for shuttle deploy

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,9 @@
-use std::net::SocketAddr;
-
 use axum::{response::IntoResponse, routing::get, Router};
 
-#[tokio::main]
-async fn main() {
+#[shuttle_runtime::main]
+async fn shuttle() -> shuttle_axum::ShuttleAxum {
     let app = Router::new().route("/", get(hello_world));
-
-    let addr = SocketAddr::new([0, 0, 0, 0].into(), 3000);
-
-    axum::Server::bind(&addr)
-        .serve(app.into_make_service())
-        .await
-        .unwrap();
+    Ok(app.into())
 }
 
 async fn hello_world() -> impl IntoResponse {
